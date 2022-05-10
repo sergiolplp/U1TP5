d12b7f14e80e7b386ed936fbdb96170ef6ee38a4
Sergio Sainz <sergiosainz.proyectos@gmail.com>
2022-05-10 00:51:22 -0300

    arreglos

---
+++ ejercicio3.js
@@ -0,0 +1,14 @@
+var fotopordefecto = 'leon1';
+
+function cambiafoto()
+{   
+    var imagen = document.getElementById('leon1');
+    if (fotopordefecto == 'leon1'){
+        imagen.src = "leon2.jpg";
+        fotopordefecto = 'leon2';
+    }
+    else {
+        imagen.src = 'leon1.jpg';
+        fotopordefecto = 'leon1';
+    }
+}
