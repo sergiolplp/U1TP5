64c7bd250145317192f183204345c9af9b772115
Sergio Sainz <sergiosainz.proyectos@gmail.com>
2022-05-10 19:12:22 -0300

    cambios

--- ejercicio4.js
+++ ejercicio4.js
@@ -1,18 +1,39 @@
-function contador()
-{
+function contador() {
     var total = 200;
-    setTimeout(function(){
+    setTimeout(function () {
         var valor = document.getElementById('contador');
         var respuesta = document.getElementById('resultado');
         var cantidad = valor.value.length;
         document.getElementById('resultado').innerText = cantidad + 'caracteres restantes.' + (total - cantidad);
 
-        if(cantidad > total)
-        {
+        if (cantidad > total) {
             respuesta.style.color = 'red';
         }
-        else{
+        else {
             respuesta.style.color = 'blue';
         }
     });
-}   
+}
+
+
+var formulario = document.getElementById('formulario');
+formulario.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    if (e.target.nombre.value == '') {
+        alert('El campo nombre es requerido');
+        return;
+    }
+
+    if (e.target.mail.value == '') {
+        alert('El campo mail es requerido');
+        return;
+    }
+
+    if (e.target.mensaje.value == '') {
+        alert('El campo mensaje es requerido');
+        return;
+    }
+
+    alert('Gracias ' + e.target.nombre.value + ' por enviarnos "' + e.target.mensaje.value + '"');
+});
